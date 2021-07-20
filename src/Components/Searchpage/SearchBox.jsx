0112c76aa05da8719c5b966896061f7c19aaca3d
Alexandre Viretti <alexou73@gmail.com>
2021-07-20 10:57:38 +0200

    feat(border): select refinement

--- src/Components/Searchpage/SearchBox.jsx
+++ src/Components/Searchpage/SearchBox.jsx
@@ -50,51 +50,51 @@ const SearchBox = ({
     );
 };
 
-const CurrentRefinements = ({ items, refine }) => {
-    const unique = uniqBy(items, 'currentRefinement');
+// const CurrentRefinements = ({ items, refine }) => {
+//     const unique = uniqBy(items, 'currentRefinement');
 
-    return (
-        <ul className="refinement-content">
-            {unique.map(item => (
-                <li className="refinement-item" key={item.label}>
-                    {item.items ? (
-                        <React.Fragment>
-                            <h3>{item.label}</h3>
-                            <ul className="refinement-results">
-                                {item.items.map(nested => (
-                                    <li key={nested.label}>
-                                        <button
-                                            className="refinement-filter"
-                                            href="#"
-                                            onClick={event => {
-                                                event.preventDefault();
-                                                refine(nested.value);
-                                            }}
-                                        >
-                                            {nested.label}
-                                        </button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </React.Fragment>
-                    ) : (
-                        <button
-                            href="#"
-                            onClick={event => {
-                                event.preventDefault();
-                                refine(item.value);
-                            }}
-                        >
-                            {item.label}
-                        </button>
-                    )}
-                </li>
-            ))}
-        </ul>
-    );
-};
+//     return (
+//         <ul className="refinement-content">
+//             {unique.map(item => (
+//                 <li className="refinement-item" key={item.label}>
+//                     {item.items ? (
+//                         <React.Fragment>
+//                             <h3>{item.label}</h3>
+//                             <ul className="refinement-results">
+//                                 {item.items.map(nested => (
+//                                     <li key={nested.label}>
+//                                         <button
+//                                             className="refinement-filter"
+//                                             href="#"
+//                                             onClick={event => {
+//                                                 event.preventDefault();
+//                                                 refine(nested.value);
+//                                             }}
+//                                         >
+//                                             {nested.label}
+//                                         </button>
+//                                     </li>
+//                                 ))}
+//                             </ul>
+//                         </React.Fragment>
+//                     ) : (
+//                         <button
+//                             href="#"
+//                             onClick={event => {
+//                                 event.preventDefault();
+//                                 refine(item.value);
+//                             }}
+//                         >
+//                             {item.label}
+//                         </button>
+//                     )}
+//                 </li>
+//             ))}
+//         </ul>
+//     );
+// };
 
-const CustomCurrentRefinements = connectCurrentRefinements(CurrentRefinements);
+// const CustomCurrentRefinements = connectCurrentRefinements(CurrentRefinements);
 
 const CustomSearchBox = connectSearchBox(SearchBox);
 
